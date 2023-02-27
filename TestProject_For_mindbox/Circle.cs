c05ffc3814a20367437a960e03203ebb68f5c62e
IOTA <IOTA@0029>
2023-02-28 00:35:00 +0500

    Добавьте файлы проекта.

---
+++ TestProject_For_mindbox/Circle.cs
@@ -0,0 +1,21 @@
+﻿namespace TestProject_For_mindbox
+{
+    public class Circle
+    {
+        double  r = 2;
+        public const double MinR = 1e-6;
+
+        public double Calculation (double r)
+        {
+            double s;
+
+            if (r - MinR < Constants.CalculationAccuracy)
+                throw new ArgumentException("Неверно указан радиус круга.", nameof(r));
+
+            s = Math.Pow(r, 2)*Math.PI;
+
+            return s;
+        }
+        public void Print() => Console.WriteLine($"Name: {Calculation}");
+    }
+}
---
+++ TestProject_For_mindbox/Tringle.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualBasic;
+using System.Reflection.Metadata;
+using System;
+
+namespace TestProject_For_mindbox
+{
+    public class Tringle
+    { 
+        double a, b, c;
+
+        public double Calculation(double a, double b, double c)
+        {
+            double s, p, perimeter;
+
+            p= 2/(a+b+c);
+
+            perimeter= a+b+c;
+            var max = Math.Max(a, Math.Max(b, c));
+            if ((perimeter - max) - max < Constants.CalculationAccuracy)
+                throw new ArgumentException("Наибольшая сторона треугольника должна быть меньше суммы других сторон");
+
+            s = Math.Sqrt(p * (p - a) * p * (p - b) * p * (p - c));
+
+            return s;
+        }
+    }
+}
